64b3ae2d69f33e3717a37c2b5cb119ec457dfd19
Maydit <Dawiided@gmail.com>
2019-03-12 19:47:28 -0400

    Add message parsing and reaction for default and seen case

--- main.js
+++ main.js
@@ -1,1 +1,67 @@
 const discord = require('discord.io');
+const auth = require('./auth.json');
+
+//All options
+const NONE = 0;
+const SEEN = 1;
+
+
+const bot = new Discord.Client({
+    token: auth.token,
+    autorun: true,
+});
+
+bot.setPresence({
+    game: {
+        name: "!pollhelp"
+    }
+});
+
+bot.on('message', async function(user, userID, channelID, message, evt) {
+    if(user.bot == true) {
+        return; //no infinite loops
+    }
+    //user wants help: print useage and options
+    if(message.indexOf("!pollhelp") != -1) {
+        bot.sendMessage({
+            to: channelID,
+            message: "Include \"!poll\" in your message to add poll reactions. Optionally add a type immediately after. Types: seen"
+        });
+        return;
+    } 
+    //main case: create a poll below a message
+    const loc = message.indexOf("!poll");
+    const mID = evt.d.id;
+    if(loc != -1) {
+        const optionPos = loc + 5;
+        let option = NONE; // enum for option
+        if(optionPos < message.length) {
+            const optionStr = message.substring(optionPos);
+            if(optionStr.toLowerCase.indexOf("seen") != -1) option = SEEN;
+        }
+        switch (option) {
+            case NONE:
+                bot.addReaction({
+                    channelID: channelID,
+                    messageID: mID,
+                    reaction: "👍"
+                });
+                bot.addReaction({
+                    channelID: channelID,
+                    messageID: mID,
+                    reaction: "👎"
+                })
+                break;
+            case SEEN:
+                bot.addReaction({
+                    channelID: channelID,
+                    messageID: mID,
+                    reaction: "👌"
+                })
+                break;
+            default:
+                break;
+        }
+        return;
+    }
+});
